52180a4cd82dd2a22bfb0d13978c92eaa83590f4
yashukaSan <ysahu1590@gmail.com>
2026-02-17 23:48:35 +0530

    refine design

--- app/flashcards/result/page.tsx
+++ app/flashcards/result/page.tsx
@@ -8,8 +8,8 @@ export default function Results(){
     const router = useRouter();
     return (
       <>
-        <div className="grid border h-screen p-20 bg-green-300">
-          <h1 className="border flex p-8 m-auto w-[60vw] justify-center text-2xl rounded-3xl bg-blue-300 text-black">
+        <div className="grid border h-screen p-20 dark:bg-purple-900 bg-green-300">
+          <h1 className="border flex p-8 m-auto w-[60vw] justify-center text-2xl rounded-3xl dark:text-blue-100 dark:bg-blue-700 bg-blue-300 text-black">
             You Score: {score}/10
           </h1>
           <button
@@ -17,7 +17,7 @@ export default function Results(){
               router.push("/flashcards/ques1");
               setScore(0);
             }}
-            className="border p-5 m-auto rounded-full sm:w-70 mt-6 sm:h-30 bg-orange-300 text-red-700 font-bold text-2xl"
+            className="border p-5 m-auto rounded-full sm:w-70 mt-6 sm:h-30 dark:bg-black bg-orange-300 text-red-700 font-bold text-2xl"
           >
             Take Test again
           </button>
@@ -26,7 +26,7 @@ export default function Results(){
               router.push("/");
               setScore(0)
             }}
-            className="border p-5 m-auto rounded-full sm:w-50 sm:h-30 bg-orange-300 text-red-700 font-bold text-2xl"
+            className="border p-5 m-auto rounded-full sm:w-50 sm:h-30 bg-orange-300 dark:bg-black text-red-700 font-bold text-2xl"
           >
             Back To Home
           </button>
--- app/flashcards/useColorTheme.tsx
+++ app/flashcards/useColorTheme.tsx
@@ -3,7 +3,7 @@ function useColorTheme(){
     const greenLight = " p-5 rounded-3xl text-center bg-green-400";
     const choices = "p-5 dark:bg-black rounded-3xl text-center";
     const mainDiv = "grid items-center justify-center dark:bg-[#101010] bg-blue-400 h-screen";
-    const parentDiv = "border-20 p-3 sm:p-14 lg:p-24 bg-[#e0e0e0] dark:bg-[#303030] text-sm lg:text-3xl lg:w-[80vw] lg:h-[70vh] rounded-tl-[30%] rounded-br-[30%] ";
+    const parentDiv = "border-20 p-3 sm:p-14 lg:p-24 bg-[#e0e0e0] dark:bg-[#303030] text-sm lg:text-3xl lg:w-[80vw] lg:h-[75vh] rounded-tl-[30%] rounded-br-[30%] mx-1";
     const moveBtn = " p-4 rounded-4xl dark:bg-blue-300 bg-orange-400 text-black font-bold font-mono"
     const quesNumClass = "sm:text-5xl text-center dark:text-[#f0f0f0] text-blue-800";
     const quesClass = "text-center m-5 sm:text-3xl font-mono text-slate-800 dark:text-zinc-300";
